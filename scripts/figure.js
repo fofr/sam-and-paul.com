1c3fc8e904fbfa3fe4aceb2bf08753cc71678881
Paul Hayes <fofr@users.noreply.github.com>
2016-10-29 13:37:46 +0100

    Revert "Don't link to imfolozi full res images"
    This reverts commit bf09f2504bd89c0707c1cfb6e6a669c370f2533b.

--- scripts/figure.js
+++ scripts/figure.js
@@ -34,7 +34,6 @@ hexo.extend.tag.register('figure', function(args, content) {
 
   var image_dir = hexo.config.image_dir,
       render = hexo.render,
-      html = '',
       image = args[0],
       orientation = args[1],
       directory = typeof args[2] === "undefined" ? "1240" : args[2],
@@ -49,12 +48,5 @@ hexo.extend.tag.register('figure', function(args, content) {
     directory = directory + '/';
   }
 
-  // No full res images uploaded yet
-  if (image.indexOf('imfolozi') !== -1) {
-    html = '<figure class="generated-figure generated-figure--retina generated-figure--620 generated-figure--'+ orientation +'"><img src="' + image_dir + directory + image +'" alt="'+ caption +'"><figcaption class="generated-figure-caption">'+ renderedCaption +'</figcaption></figure>';
-  } else {
-    html = '<figure class="generated-figure generated-figure--retina generated-figure--620 generated-figure--'+ orientation +'"><a href="' + image_dir + image +'"><img src="' + image_dir + directory + image +'" alt="'+ caption +'"></a><figcaption class="generated-figure-caption">'+ renderedCaption +'</figcaption></figure>'
-  }
-
-  return html;
+  return '<figure class="generated-figure generated-figure--retina generated-figure--620 generated-figure--'+ orientation +'"><a href="' + image_dir + image +'"><img src="' + image_dir + directory + image +'" alt="'+ caption +'"></a><figcaption class="generated-figure-caption">'+ renderedCaption +'</figcaption></figure>';
 }, {ends: true});
